7d9a8a6cfbca41b448a3a25e1ac84ec46b636e4a
differental <enderchen004@outlook.com>
2024-03-02 11:51:19 +0000

    test

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from transformers import pipeline
 
 pipe = pipeline("text-generation", model="HuggingFaceH4/zephyr-7b-beta", torch_dtype=torch.bfloat16, device_map="auto")
 
+print(pipeline('text-generation',model="HuggingFaceH4/zephyr-7b-beta", torch_dtype=torch.bfloat16, device_map="auto")('Generate a mechanics problem for me'))
+
+print("model initialised")
 # We use the tokenizer's chat template to format each message - see https://huggingface.co/docs/transformers/main/en/chat_templating
 messages = [
     {
@@ -11,6 +14,9 @@ messages = [
     },
     {"role": "user", "content": "How many helicopters can a human eat in one sitting?"},
 ]
+print("message initialised")
 prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-outputs = pipe(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
+print("prompt initialised")
+outputs = pipe(prompt,  pad_token_id=50256, max_length=10, num_return_sequences=1, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
+print("output initialised")
 print(outputs[0]["generated_text"])
