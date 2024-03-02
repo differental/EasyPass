221691999ab6414ef309000506842ce4e3d6cf8b
differental <enderchen004@outlook.com>
2024-03-02 14:41:40 +0000

    added not working code

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-import torch
-from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
-pipe = pipeline("text-generation", model="HuggingFaceH4/zephyr-7b-beta", torch_dtype=torch.bfloat16, device_map="auto")
+#tokenizer = AutoTokenizer.from_pretrained("google/gemma-7b")
+#model = AutoModelForCausalLM.from_pretrained("google/gemma-7b")
 
-print(pipeline('text-generation',model="HuggingFaceH4/zephyr-7b-beta", torch_dtype=torch.bfloat16, device_map="auto")('Generate a mechanics problem for me'))
 
-print("model initialised")
-# We use the tokenizer's chat template to format each message - see https://huggingface.co/docs/transformers/main/en/chat_templating
-messages = [
-    {
-        "role": "system",
-        "content": "You are a friendly chatbot who always responds in the style of a pirate",
-    },
-    {"role": "user", "content": "How many helicopters can a human eat in one sitting?"},
-]
-print("message initialised")
-prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-print("prompt initialised")
-outputs = pipe(prompt,  pad_token_id=50256, max_length=10, num_return_sequences=1, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
-print("output initialised")
-print(outputs[0]["generated_text"])
+tokenizer = AutoTokenizer.from_pretrained("HuggingFaceH4/zephyr-7b-beta")
+model = AutoModelForCausalLM.from_pretrained("HuggingFaceH4/zephyr-7b-beta")
+
+input_text = "Generate a mechanics problem that involves angular momentum and moment of inertia, only output the problem and nothing else."
+input_ids = tokenizer(input_text, return_tensors="pt")
+
+outputs = model.generate(**input_ids)
+print(tokenizer.decode(outputs[0]))
