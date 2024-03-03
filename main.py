dfbece8e070f252f2f130360c9cfb975f88bd347
Brian Chen <enderchen004@outlook.com>
2024-03-03 00:09:28 +0000

    Seems this works quite well

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-#tokenizer = AutoTokenizer.from_pretrained("google/gemma-7b")
-#model = AutoModelForCausalLM.from_pretrained("google/gemma-7b")
-
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 tokenizer = AutoTokenizer.from_pretrained("HuggingFaceH4/zephyr-7b-beta")
-model = AutoModelForCausalLM.from_pretrained("HuggingFaceH4/zephyr-7b-beta")
+
+model = AutoModelForCausalLM.from_pretrained("HuggingFaceH4/zephyr-7b-beta", low_cpu_mem_usage=True)
+model.load_adapter("brianc07/easypass4")
 
 input_text = "Generate a mechanics problem that involves angular momentum and moment of inertia, only output the problem and nothing else."
 input_ids = tokenizer(input_text, return_tensors="pt")
 
-outputs = model.generate(**input_ids)
+outputs = model.generate(**input_ids, max_new_tokens=256)
 print(tokenizer.decode(outputs[0]))
+
+#config = LoraConfig(lora_alpha=16, lora_dropout=0.1, r=64, bias="none", task_type="CASUAL_LM")
+#model.add_adapter(peft_config)
+
+
+
+
